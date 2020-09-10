f899073e2ad03643f5eab40b82b94bcd9c1a41bd
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-10 10:15:22 +0200

    added dummy testcases

---
+++ src/app/components/project/testcases.ts
@@ -0,0 +1,102 @@
+import {Testcase} from './testcase';
+
+export const TESTCASES: Testcase[] = [
+  {
+    id: "01",
+    name: 'test01',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "02",
+    name: 'test02',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "03",
+    name: 'test03',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "04",
+    name: 'test04',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "05",
+    name: 'test05',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "06",
+    name: 'test06',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "07",
+    name: 'test07',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "08",
+    name: 'test08',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "09",
+    name: 'test09',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "10",
+    name: 'test10',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "11",
+    name: 'test11',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "12",
+    name: 'test12',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "13",
+    name: 'test13',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+  {
+    id: "14",
+    name: 'test14',
+    state: 0,
+    description: "test description",
+    steps: []
+  },
+];
