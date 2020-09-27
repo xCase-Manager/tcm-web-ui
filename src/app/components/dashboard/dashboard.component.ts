6ec6c620139fab82229693ae53dffdb101a8aefe
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-27 12:49:44 +0200

    added data loader for the dashboard chart

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+import {TESTCASES} from '../project/testcases';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  chartLabels: Label[] = [];
+  chartData: MultiDataSet[0] = [];
+  chartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    var projects = TESTCASES.map(item => item.projectId).filter((v, i, a) => a.indexOf(v) === i);
+    projects.forEach(unique => {
+      var countOccurrences = TESTCASES.reduce((a, v) => (v.projectId === unique ? a + 1 : a), 0);
+      this.chartLabels.push(unique);
+      this.chartData.push(countOccurrences);
+    });
+      
   }
+}
 
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
 }
