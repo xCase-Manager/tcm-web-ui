9714889bedd0b982f6eccdaecc8b9c883dbda4ca
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-22 19:19:48 +0200

    added test assertions for projects table list

--- src/app/components/project/projectsList/projects-list.component.spec.ts
+++ src/app/components/project/projectsList/projects-list.component.spec.ts
@@ -10,9 +10,18 @@ describe('ProjectsListComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app projects list', () => {
+  it('should render the projects list component', () => {
     const fixture = TestBed.createComponent(ProjectsListComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render the project lists table', () => {
+    const fixture = TestBed.createComponent(ProjectsListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('th').textContent).toContain('Project');
+    expect(compiled.querySelector('th').textContent).toContain('Description');
+    expect(compiled.querySelector('th').textContent).toContain('Status');
   });
 });
